acee4e07b5e3b6b67d783b3eeda3728b5172fe3a
Stephen Brown <stephen.jr36@gmail.com>
2024-08-12 15:11:01 -0600

    completed win condition

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css';
 import getRandomWord from './src/randomWord.js';
 import setSharkImage from './src/sharkImage.js';
 import { setupWord, isLetterInWord, revealLetterInWord } from './src/word.js';
+import setupGuesses from './src/guess.js';
 
 
 document.querySelector('#app').innerHTML = `
@@ -24,8 +25,25 @@ const initSharkwords = () => {
 
   setupWord(document.querySelector('#word-container'), word);
 
-  revealLetterInWord(word[2])
+  // QUESTION: Is it best to have this function declaration inside our outside initSharkwords delcaration?
+  const handleGuess = (guessEvent, letter) => {
+    guessEvent.target.setAttribute('disabled', true);
+    if (isLetterInWord(letter)) {
+      revealLetterInWord(letter);
+    } else {
+      numWrong++;
+      setSharkImage(document.querySelector('#shark-img'), numWrong);
+    };
 
+    const isWordComplete = [...document.querySelectorAll('.letter-box')].every((el) => el.innerText !== '');
+
+    if (isWordComplete) {
+      document.querySelector('#game-status').innerText = `You win!`;
+      document.querySelectorAll('button').forEach((letterBox) => letterBox.setAttribute('disabled', true))
+    }
+  };
+
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess)
 };
 
 initSharkwords();
--- src/guess.js
+++ src/guess.js
@@ -1,10 +1,16 @@
+// Create a string of the values to be used as buttons for user input
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
+// This function takes the HTML element and a callback function as arguments
 function setupGuesses(element, handleGuess) {
+  // Takes alphabet string and turns it into an array
   alphabet.split('').forEach((letter) => {
+    // Creates a button element with the innerText of the current letter
     const btn = document.createElement('button');
     btn.innerText = letter;
+    // Adds an event listener to the button created
     btn.addEventListener('click', (e) => handleGuess(e, letter));
+    // Appends the button to the HTML element
     element.append(btn);
   });
 }
